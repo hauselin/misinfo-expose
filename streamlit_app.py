018d8ff3da8128553a19aaa89d214fecaeecf03e
hauselin <hauselin@gmail.com>
2022-11-17 11:34:48 -0500

    Add pie chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 #%%
 
+import altair as alt
+import numpy as np
 import requests
 import streamlit as st
-import json
+import pandas as pd
 
 #%%
 
@@ -18,9 +20,11 @@ def get_data(screen_name):
 
 #%%
 
+st.set_page_config(page_title="Misinformation exposure")
+
 st.markdown("### How much misinformation are you exposed to?")
 
-st.write("Details/info about the app")
+st.write("Details/info about the app here")
 
 screen_name = st.text_input("Enter your Twitter username or ID to find out.")
 
@@ -40,14 +44,33 @@ if screen_name:
             f"You entered **{data['twitter_screen_name']}** (ID: {data['twitter_user_id']})."
         )
 
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Exposure", value=data["misinfo_exposure_score"], delta="1.2%")
-        col2.metric(
+        columns = st.columns(3)
+        columns[0].metric(
+            "Exposure", value=data["misinfo_exposure_score"], delta="1.2%"
+        )
+        columns[1].metric(
             "Weighted exposure",
             value=data["misinfo_exposure_score_weighted_numtweets"],
             delta="-1.2%",
         )
-        col3.metric("Partisanship", value=data["partisan_score"], delta="1.2%")
+        columns[2].metric("Partisanship", value=data["partisan_score"], delta="1.2%")
+
+        st.write("add figures below")
+        source = pd.DataFrame({"category": [1, 2], "value": [1, 2]})
+        c1 = (
+            alt.Chart(source)
+            .mark_arc(outerRadius=100)
+            .encode(
+                theta=alt.Theta(field="value", type="quantitative"),
+                color=alt.Color(
+                    field="category",
+                    type="nominal",
+                    scale=alt.Scale(range=["#09ab3b", "#ff4141"]),
+                    legend=None,
+                ),
+            )
+        )
+        st.altair_chart(c1, use_container_width=True)
 
         st.markdown(
             f"The estimates above are based on these **{data['num_following']}** users **{data['twitter_screen_name'].lower()}** follows:"
@@ -55,3 +78,6 @@ if screen_name:
         st.json(data["following"])
 
     # st.write(data)
+
+
+#%%
